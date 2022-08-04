452cdf2f21e03652e8d523e5238b5cf2393bdff2
Larisa <106819927+LarisaShirokikh@users.noreply.github.com>
2022-08-04 20:25:00 +0300

    posts

--- src/middlewares/title-validation.ts
+++ src/middlewares/title-validation.ts
@@ -5,31 +5,31 @@ const regexp = new RegExp('^https://([a-zA-Z0-9_-]+\\.)+[a-zA-Z0-9_-]+(\\/[a-zA-
 
 export const titleValidationCreate = body('title').trim().isLength({
     max: 100
-}).withMessage('Title should be from 5 to 30 symbols');
+});
 
 
 
  export const titleValidationBloggersPosts = body('title').trim().isLength({
     max: 30
-}).withMessage('Title should be from 5 to 30 symbols');
+});
 
 export const shortDescriptionValidationBloggersPosts = body('shortDescription').trim().isLength({
     max: 100
-}).withMessage('shortDescription should be from 5 to 100 symbols');
+});
 
 export const shortDescriptionValidation = body('shortDescription').trim().isLength({
 
     max: 100
-}).withMessage('shortDescription should be from 0 to 100 symbols');
+});
 
 export const contentValidation = body('content').trim().isLength({
 
     max: 1000
-}).withMessage('content should be max to 1000 symbols');
+});
 
 export const nameValidationCreate = body('name').trim().isLength({
     max: 15
-}).withMessage('Name should be from 0 to 15 symbols');
+});
 
 export const urlValidation = body('youtubeUrl').trim().isLength({
     max: 100
