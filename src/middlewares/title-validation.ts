e555bd17be5c454b6f4768ddddfb3d4a4939d915
Larisa <106819927+LarisaShirokikh@users.noreply.github.com>
2022-08-05 15:08:08 +0300

    onlyFirstError2

--- src/middlewares/title-validation.ts
+++ src/middlewares/title-validation.ts
@@ -18,22 +18,22 @@ export const shortDescriptionValidationBloggersPosts = body('shortDescription').
 });
 
 export const shortDescriptionValidation = body('shortDescription').trim().isLength({
-
+ min: 0,
     max: 100
 });
 
 export const contentValidation = body('content').trim().isLength({
-
+ min: 0,
     max: 1000
 });
 
 export const nameValidationCreate = body('name').trim().isLength({
-    min: 4,
+    min: 0,
     max: 15
 });
 
 export const urlValidation = body('youtubeUrl').trim().isLength({
-    
+    min: 0,
     max: 100
 }).matches(regexp)
 
