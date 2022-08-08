b33e44881d93ab5fd90ceeaf780f76d564f850b3
Larisa <106819927+LarisaShirokikh@users.noreply.github.com>
2022-08-08 10:32:19 +0300

    validation

--- src/middlewares/title-validation.ts
+++ src/middlewares/title-validation.ts
@@ -8,36 +8,29 @@ const regexp = new RegExp("^https://([a-zA-Z0-9_-]+\.)+[a-zA-Z0-9_-]+(\/[a-zA-Z0
 
 export const titleValidationCreate = body('title').trim().isLength({
     min: 1,
-    max: 29
-});
-
-
-
- export const titleValidationBloggersPosts = body('title').trim().isLength({
-     min: 1,
-    max: 29
-});
+    max: 30
+}).isString();
 
 
 export const shortDescriptionValidation = body('shortDescription').trim().isLength({
     min: 1,
     max: 100
-});
+}).isString();
 
 export const contentValidation = body('content').trim().isLength({
     min: 1,
     max: 1000
-});
+}).isString();
 
 export const nameValidationCreate = body('name').trim().isLength({
     min: 1,
     max: 15
-});
+}).isString();
 
 export const urlValidation = body('youtubeUrl').trim().isLength({
     min: 1,
     max: 100
-}).matches(regexp)
+}).matches(regexp).isString()
 
 
 
